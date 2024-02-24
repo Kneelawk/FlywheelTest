4cb3cfed804833df33aaad098a2a7007d917c182
Kneelawk <kneelawk@gmail.com>
2024-02-24 03:08:46 -0800

    Initial cursed texture loading

--- forge/src/main/java/com/kneelawk/flywheeltest/forge/client/visual/VRMPlayerVisual.java
+++ forge/src/main/java/com/kneelawk/flywheeltest/forge/client/visual/VRMPlayerVisual.java
@@ -3,6 +3,7 @@ package com.kneelawk.flywheeltest.forge.client.visual;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -21,11 +22,19 @@ import com.jozufozu.flywheel.lib.visual.SimpleEntityVisual;
 
 import net.minecraftforge.fml.loading.FMLLoader;
 
+import org.lwjgl.system.MemoryUtil;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+
 import org.joml.Vector4f;
 
+import com.mojang.blaze3d.platform.NativeImage;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 
@@ -88,12 +97,44 @@ public class VRMPlayerVisual extends SimpleEntityVisual<Player> {
         // really hacky bounding sphere
         Vector4f boundingSphere = new Vector4f(0f, 1f, 0f, 1f);
 
+        Int2ObjectMap<ResourceLocation> image2RL = new Int2ObjectOpenHashMap<>();
+
         ImmutableMap.Builder<Material, Mesh> modelBuilder = ImmutableMap.builder();
         for (GLTFData.GLTFMesh mesh : data.data.meshes) {
             for (GLTFData.GLTFPrimitive primitive : mesh.primitives) {
-                // extremely cursed way to allow multiple meshes with the same material
+                GLTFData.GLTFMaterial gltfMaterial = data.data.materials[primitive.material];
+                int colorIndex = gltfMaterial.pbrMetallicRoughness.baseColorTexture.index;
+
+                ResourceLocation textureId = image2RL.computeIfAbsent(colorIndex, _colorIndex -> {
+                    GLTFData.GLTFImage image = data.data.images[colorIndex];
+                    GLTFData.GLTFBufferView view = data.data.bufferViews[image.bufferView];
+
+//                    var future = CompletableFuture.supplyAsync(() -> {
+                    ByteBuffer imageBuf = MemoryUtil.memAlignedAlloc(8, view.byteLength);
+                    try {
+                        imageBuf.put(data.binaryData, view.byteOffset, view.byteLength);
+                        imageBuf.rewind();
+                        NativeImage nativeImage = NativeImage.read(imageBuf);
+                        DynamicTexture imageTexture = new DynamicTexture(nativeImage);
+                        // FIXME: this probably leaks memory like crazy
+                        return Minecraft.getInstance().textureManager.register(image.name, imageTexture);
+                    } catch (IOException e) {
+                        System.err.println("Error loading " + image.name);
+                        return new ResourceLocation("missing");
+                    } finally {
+                        MemoryUtil.memAlignedFree(imageBuf);
+                    }
+//                    }, Minecraft.getInstance());
+//                    try {
+//                        return future.get();
+//                    } catch (InterruptedException | ExecutionException e) {
+//                        throw new RuntimeException(e);
+//                    }
+                });
+
                 Material material =
-                    SimpleMaterial.builder().cutout(CutoutShaders.ONE_TENTH).mipmap(false).diffuse(false).build();
+                    SimpleMaterial.builder().cutout(CutoutShaders.HALF).mipmap(false).diffuse(false).texture(textureId)
+                        .build();
 
                 modelBuilder.put(material, new VRMMesh(data.data, data.binaryData, primitive, boundingSphere));
             }
